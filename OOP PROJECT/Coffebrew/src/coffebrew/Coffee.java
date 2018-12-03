c72f202a71e96d9d5c9ca386d31bf3dadb47fa91
1Bibz <36074549+1Bibz@users.noreply.github.com>
2018-12-03 00:40:58 +0000

    Add files via upload

---
+++ OOP PROJECT/Coffebrew/src/coffebrew/Coffee.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coffebrew;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author x17333376
+ */
+public class Coffee implements Serializable{
+    private String receipt;
+
+    public Coffee() {
+        receipt=new String();
+    }
+
+    
+
+    public String getReceipt() {
+        return receipt;
+    }
+
+    public void setReceipt(String receipt) {
+        this.receipt = receipt;
+    }
+
+   
+
+   
+    
+    
+    
+    
+
+    
+}
